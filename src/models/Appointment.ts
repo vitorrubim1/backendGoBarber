3f8f25362647d830cff33eed3acab33fac022c5b
Vitor Rubim <Vitorrubim1@hotmail.com>
2021-03-04 17:38:43 -0300

    chore: criando o model de agendamento

--- src/models/Appointment.ts
+++ src/models/Appointment.ts
@@ -1,18 +1,30 @@
-import { v4 as uuid } from 'uuid';
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+} from 'typeorm'; /*
+  Entity: entidade que será salva no banco de dados
+  Column: colunas do banco de dados
+  PrimaryGeneratedColumn: para a coluna id, por ser primario e gerado automaticamente
+*/
 
+// Model está relacionado com uma tabela do banco de dados
+
+@Entity(
+  'appointments',
+) /*
+  @Entity: é um decorator, que funciona como uma função e como parametro enviará a classe Appointment abaixo
+  'appointments': tabela do banco de dados
+*/
 class Appointment {
+  @PrimaryGeneratedColumn('uuid')
   id: string;
 
+  @Column()
   provider: string;
 
+  @Column('time with time zone')
   date: Date;
-
-  constructor({ provider, date }: Omit<Appointment, 'id'>) {
-    // Omit: é uma função helper que ajuda a omitir uma informação, o id eu não preciso então digo que dentro de Appointment, o dado q quero omitir é o id
-    this.id = uuid();
-    this.provider = provider;
-    this.date = date;
-  }
 }
 
 export default Appointment;
