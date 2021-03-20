c8118599fff94b18d1a90e9230f32e31aa704ae8
Vitor Rubim <Vitorrubim1@hotmail.com>
2021-03-20 13:57:15 -0300

    feat: criando uma rota para que os arquivos estaticos possam ser requisitados pelo front-end

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import 'reflect-metadata'; // typeorm exige por conta que utilizamos decorators
-
 import express from 'express';
+
+import uploadConfig from './config/upload'; // arquivo de configuração de upload de imagem
 
 import routes from './routes';
 import './database';
@@ -8,7 +9,7 @@ import './database';
 const app = express();
 
 app.use(express.json());
-
+app.use('/files', express.static(uploadConfig.directory)); // servindo pra aplicação de forma estática o arquivo físico, as imagens
 app.use(routes); // usando o arquivo index de rotas
 
 app.listen(3333, () => {
