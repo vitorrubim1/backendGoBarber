499ec32e085449b939da3966db063af357458d0f
Vitor Rubim <Vitorrubim1@hotmail.com>
2021-04-03 21:59:21 -0300

    breaking change: Dependency Inversion Principle(SOLID), Ao invés do service lidar diretamente com o repositório importado no arquivo do service, iremos receber esse mesmo repositório como parâmetro do constructor desse service passando o tipo dele como sendo a interface criada para implementar no próprio repositório.

--- src/modules/appointments/infra/http/routes/appointments.routes.ts
+++ src/modules/appointments/infra/http/routes/appointments.routes.ts
@@ -1,5 +1,4 @@
 import { Router } from 'express';
-import { getCustomRepository } from 'typeorm';
 import { parseISO } from 'date-fns'; // isEqual: pra ver se é igual, mesma data e mesmo horário
 
 import AppointmentsController from '@modules/appointments/infra/typeorm/controllers/AppointmentsController';
@@ -8,24 +7,26 @@ import CreateAppointmentService from '@modules/appointments/services/CreateAppoi
 import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated'; // middleware de validação de autenticação
 
 const appointmentsRouter = Router();
+const appointmentsController = new AppointmentsController(); // instanciando
 
 appointmentsRouter.use(ensureAuthenticated); // para que todas as rotas usem a validação de autenticação
 
 // middlewares
 
-appointmentsRouter.get('/', async (request, response) => {
-  const appointmentsController = getCustomRepository(AppointmentsController);
-  const appointments = await appointmentsController.find(); // metódo de listagem dentro do controller
+// appointmentsRouter.get('/', async (request, response) => {
+//   const appointments = await appointmentsController.find(); // metódo de listagem dentro do controller
 
-  return response.json(appointments);
-});
+//   return response.json(appointments);
+// });
 
 appointmentsRouter.post('/', async (request, response) => {
   const { provider_id, date } = request.body;
 
   const parsedDate = parseISO(date); // transforma de string em data
 
-  const createAppointment = new CreateAppointmentService();
+  const createAppointment = new CreateAppointmentService(
+    appointmentsController,
+  );
 
   const appointment = await createAppointment.execute({
     date: parsedDate,
--- src/modules/appointments/services/CreateAppointmentService.ts
+++ src/modules/appointments/services/CreateAppointmentService.ts
@@ -1,28 +1,32 @@
 import { startOfHour } from 'date-fns';
-import { getCustomRepository } from 'typeorm'; // getCustomRepository: função pra buscar um controller
 
 import AppError from '@shared/errors/AppError'; // classe de erros
 
-import Appointment from '../infra/typeorm/entities/Appointment';
-import AppointmentsController from '../infra/typeorm/controllers/AppointmentsController';
+import IAppointmentController from '../controllers/IAppointmentController'; // interface com métodos não dependentes do typeorm
 
-interface RequestDTO {
+import Appointment from '../infra/typeorm/entities/Appointment';
+
+interface IRequest {
   provider_id: string;
   date: Date;
 }
 
 class CreateAppointmentService {
+  // SOLID: D: DEPENDENCY INVERSION
+  /*
+  basicamente, inverteremos as obrigações, a rota que for utilizar este service precisará passar o repositório nos parâmetros,
+  assim tendo que tipar esse repositório com a interface IAppointmentController criada para substituir os métodos do typeorm
+  */
+
+  constructor(private appointmentsRepository: IAppointmentController) {}
+
   // executando a criação de um novo agendamento. : Appointment = oq preciso retornar
-  public async execute({
-    date,
-    provider_id,
-  }: RequestDTO): Promise<Appointment> {
+  public async execute({ date, provider_id }: IRequest): Promise<Appointment> {
     // Promise<>: pq a função é assincrona
-    const appointmentsRepository = getCustomRepository(AppointmentsController);
 
     const appointmentDate = startOfHour(date); // pra q seja agendado de hora em hora
 
-    const findAppointmentsInSameDate = await appointmentsRepository.findByDate(
+    const findAppointmentsInSameDate = await this.appointmentsRepository.findByDate(
       appointmentDate,
     ); // passando pro metodo dentro do controller, a data formatada
 
@@ -31,12 +35,11 @@ class CreateAppointmentService {
       throw new AppError('This appointment is already booked'); // criando um erro, pq não temos acesso ao request, response
     }
 
-    const appointment = await appointmentsRepository.create({
+    const appointment = await this.appointmentsRepository.create({
       // esse metódo só cria a instância do model
       provider_id,
       date: appointmentDate,
     }); // chamando o metódo de criação e passando os parametros
-
 
     return appointment;
   }
