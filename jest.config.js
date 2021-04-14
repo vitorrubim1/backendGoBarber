98d7e688f678817c458338365ccf36755e37ca0a
Vitor Rubim <Vitorrubim1@hotmail.com>
2021-04-14 12:05:33 -0300

    feat: configurando o coverage report no Jest. Essa ferramenta nos auxilia bastante pois nos mostra qual porcentagem de linhas estamos testando, quais linhas foram testadas e quais não foram e isso serve para nos guiar durante a criação dos testes. Dessa maneira, não criamos testes a mais ou a menos que o necessário.

--- jest.config.js
+++ jest.config.js
@@ -15,13 +15,13 @@ module.exports = {
   clearMocks: true,
 
   // Indicates whether the coverage information should be collected while executing the test
-  // collectCoverage: false,
+  collectCoverage: true,
 
   // An array of glob patterns indicating a set of files for which coverage information should be collected
-  // collectCoverageFrom: undefined,
+  collectCoverageFrom: ['<rootDir>/src/modules/**/services/*.ts'], // caminhos dos arquivos que quero que sejam monitorados para serem testados
 
   // The directory where Jest should output its coverage files
-  // coverageDirectory: undefined,
+  coverageDirectory: 'coverage', // nome do arquivo que será criado
 
   // An array of regexp pattern strings used to skip coverage collection
   // coveragePathIgnorePatterns: [
@@ -32,12 +32,7 @@ module.exports = {
   // coverageProvider: 'v8',
 
   // A list of reporter names that Jest uses when writing coverage reports
-  // coverageReporters: [
-  //   "json",
-  //   "text",
-  //   "lcov",
-  //   "clover"
-  // ],
+  coverageReporters: ['text-summary', 'lcov'],
 
   // An object that configures minimum threshold enforcement for coverage results
   // coverageThreshold: undefined,
