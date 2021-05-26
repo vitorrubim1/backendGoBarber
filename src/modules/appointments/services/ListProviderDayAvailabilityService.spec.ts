f110a4e150fbc2ae1ef01aef3fe6308de31922ca
Vitor Rubim <Vitorrubim1@hotmail.com>
2021-05-25 22:13:17 -0300

    feat: impedindo que um agendamento seja feito em uma data passada

--- src/modules/appointments/services/ListProviderDayAvailabilityService.spec.ts
+++ src/modules/appointments/services/ListProviderDayAvailabilityService.spec.ts
@@ -18,32 +18,40 @@ describe('ListProviderMonthAvailability', () => {
     ); // criando o service, e recebendo o repositório fake, pra testes
   });
 
-  // exibir os dias disponives de um provedor
+  // exibir os dias disponíveis de um provedor
   it('should be able to list the day availability from provider', async () => {
     // criando agendamentos
     await fakeAppointmentsRepository.create({
       provider_id: 'user',
-      date: new Date(2021, 4, 20, 8, 0, 0),
+      date: new Date(2021, 4, 20, 14, 0, 0),
     });
 
     await fakeAppointmentsRepository.create({
       provider_id: 'user',
-      date: new Date(2021, 4, 20, 10, 0, 0),
+      date: new Date(2021, 4, 20, 15, 0, 0),
+    });
+
+    // verificando se um horário que já passou não está mais disponível
+    jest.spyOn(Date, 'now').mockImplementationOnce(() => {
+      return new Date(2021, 4, 20, 11).getTime(); // 11h
     });
 
     const availability = await listProviderDayAvailabilityService.execute({
       provider_id: 'user',
-      day: 20,
-      month: 5,
       year: 2021,
+      month: 5,
+      day: 20,
     });
 
     expect(availability).toEqual(
       expect.arrayContaining([
         { hour: 8, available: false }, // já tem nesse horário então é pra dar false
-        { hour: 9, available: true }, // não tem então é true
+        { hour: 9, available: false },
         { hour: 10, available: false },
-        { hour: 11, available: true },
+        { hour: 13, available: true },
+        { hour: 14, available: false },
+        { hour: 15, available: false },
+        { hour: 16, available: true },
       ]),
     );
     // espero que o availability seja igual a um array, arrayContaining: verá se a resposta do availability é um array contendo o que é passado dentro do paramêtro
--- src/modules/appointments/services/ListProviderDayAvailabilityService.ts
+++ src/modules/appointments/services/ListProviderDayAvailabilityService.ts
@@ -1,7 +1,7 @@
 // listar as horas disponíveis de um prestador em um mês
 
 import { injectable, inject } from 'tsyringe';
-import { getHours } from 'date-fns'; // getDaysInMonth: quantos dias tem no mês, getDate: retorna o dia
+import { getHours, isAfter } from 'date-fns'; // getDaysInMonth: quantos dias tem no mês, getDate: retorna o dia
 
 import IAppointmentRepository from '../repositories/IAppointmentRepository';
 
@@ -49,16 +49,19 @@ class ListProviderDayAvailabilityService {
       (_, index) => index + hourStart, // pra começar as 8h da manhã
     ); // vai ter um length de 10, pq só é possível em um dia 10 agendamentos
 
-    // verificando quais são as horas disponíveis
+    const currentDate = new Date(Date.now());
 
+    // verificando quais são as horas disponíveis
     const availability = eachArray.map(hour => {
       const hasAppointmentInHour = appointments.find(
         appointment => getHours(appointment.date) === hour,
       );
 
+      const compareDate = new Date(year, month - 1, day, hour); // informações que recebo por parâmetro
+
       return {
         hour,
-        available: !hasAppointmentInHour,
+        available: !hasAppointmentInHour && isAfter(compareDate, currentDate), // se a hora do parâmetro já passou, não terá mais disponibilidade
       };
     });
 
